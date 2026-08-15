b7e7fd78867158448bbf2a16475830fbce451c5f
4535992 <tentimarco0@gmail.com>
2026-08-15 11:08:59 +0200

    Fix RuboCop documentation for ONLYOFFICE migration

--- db/migrate/20260802000001_add_onlyoffice_key_to_dmsf_file_revisions.rb
+++ db/migrate/20260802000001_add_onlyoffice_key_to_dmsf_file_revisions.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Adds the persisted ONLYOFFICE document key used to deduplicate save callbacks.
 class AddOnlyofficeKeyToDmsfFileRevisions < ActiveRecord::Migration[7.0]
   def change
     add_column :dmsf_file_revisions, :onlyoffice_key, :string, limit: 128
